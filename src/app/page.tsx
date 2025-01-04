a12736aa0bf92ff1e96b731ef211da74d68c98e5
Masoud Moradi <147343920+massi-web-app@users.noreply.github.com>
2025-01-04 15:21:45 +0300

    implement get result Courses and log (basic)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import  Image from 'next/image'
-import {Button} from './_components/button'
 import {HomeHeroSection} from "@/app/_components/home-hero-section/home-hero-section";
+import {CourseSummeryInterface} from "@/types/course-summery.interface";
 
-export default function Home() {
+
+async function getNewestCourses(count: number): Promise<CourseSummeryInterface[]> {
+    const res = await fetch(`https://api.classbon.com/api/courses/newest/${count}`, {
+        next: {
+            revalidate: 24 * 60 * 60
+        }
+    });
+    return res.json();
+}
+
+
+export default async function Home() {
+    const newestCourses = await getNewestCourses(4);
     return (
         <>
             <HomeHeroSection/>
-            <div className="container">
-            </div>
+            {
+                newestCourses.map((p) => (
+                    <p key={p.title}>{p.title}</p>
+                ))
+            }
         </>
     );
 }
