be046ed1f6e4d087cc82987e03bf5106a9e0f68f
Masoud Moradi <147343920+massi-web-app@users.noreply.github.com>
2025-02-03 16:01:00 +0300

    implement course details

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -3,7 +3,7 @@ import {API_URL} from "@/configs/global";
 
 
 export async function generateStaticParams() {
-    const slugs =await  fetch(`${API_URL}/courses/slugs`).then((res) =>
+    const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
         res.json()
     );
 
@@ -20,12 +20,32 @@ export async function getCourse(slug: string): Promise<CourseDetails> {
 const CourseDetails = async ({params}: { params: { slug: string } }) => {
 
     const {slug} = params;
-    const courseData = await getCourse(slug);
+    const course = await getCourse(slug);
 
 
     return (
-        <div className="flex-1 flex justify-center items-center text-5xl">
-            <h1>{courseData.title }</h1>
+        <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+            <div
+                className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
+
+            <div className="col-span-10 xl:col-span-7">
+                <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+                    {course.title}
+                </h1>
+                <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+                    {course.subTitle}
+                </h2>
+                <div className=" mt-5">Video Player Component</div>
+            </div>
+
+            <div className="col-span-10 xl:col-span-3 bg-secondary">
+            </div>
+
+            <div className="col-span-10 xl:col-span-6 bg-info">
+            </div>
+
+            <div className="col-span-10 xl:col-span-4 bg-warning">
+            </div>
         </div>
     )
 }
