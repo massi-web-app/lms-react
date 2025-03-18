7d1560e34990f2c99f305263b9701f7c79d8286d
masoud <masoud.moradi.developer@gmail.com>
2025-03-18 12:59:29 -0700

    install next auth js && config next auth

---
+++ src/auth.config.ts
@@ -0,0 +1,10 @@
+import {NextAuthConfig} from "next-auth";
+
+export const authConfig = {
+    pages: {
+        signIn: '/signin',
+    },
+    callbacks: {},
+    providers: []
+} satisfies NextAuthConfig;
+
